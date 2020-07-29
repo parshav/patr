b187c630ec293303e51482f771efe9b5c36bdf4c
pv <parshav@live.com>
2020-07-28 21:22:18 -0500

    Useless config struct added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+// Is there a different way o store config.
+// Separate out the writing function
+
 fn main() {
-    let mut text = String::new();
+    let current_directory = env::current_dir()
+        .expect("Error in current directory")
+        .to_str()
+        .expect("Error in converting to string");
+    // check for current diredctory
+
+    //let config_exists = Path::new(format!("{}/.patr/config", current_directory).as_str());
 }
 
+// checks for config, if exists, use that delim.
+// else create new,
+fn config_check_for_delim() -> Config {
+	Config {
+		name: String::from("hi"),
+		delim: 'a'
+	}
+}
+
+struct Config {
+    name: String,
+    delim: char,
+}
 /*
 TODO
 - patr day5 // patr [title]
