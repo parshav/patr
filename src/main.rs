84550b2738e5733dc4b8460b7fb7269953303d2f
pv <parshav@live.com>
2020-07-28 22:03:30 -0500

    Buffer until init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
-use std::fs;
-use std::path::Path;
+use std::io::{self, BufRead};
 
-// Is there a different way o store config.
+// Is there a different way to store config, even from writing down config?
 // Separate out the writing function
 
 fn main() {
@@ -10,18 +9,36 @@ fn main() {
         .expect("Error in current directory")
         .to_str()
         .expect("Error in converting to string");
-    // check for current diredctory
 
-    //let config_exists = Path::new(format!("{}/.patr/config", current_directory).as_str());
+    // check for current diredctory
+    // Read argument
+    // start the writing -> use read [option] or rust built in
+
+    // maybe better way to d this ?
+    let title: String = env::args()
+    	.into_iter()
+    	.skip(1)
+    	.nth(0)
+    	.expect("Error in getting argument");
+
+    println!("Title is {}", title);
+
+	let mut buffer = vec![];
+	let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_until(b'-', &mut buffer)
+    	.expect("Error in read until");
+
 }
 
 // checks for config, if exists, use that delim.
 // else create new,
 fn config_check_for_delim() -> Config {
-	Config {
-		name: String::from("hi"),
-		delim: 'a'
-	}
+    Config {
+        name: String::from("Notebook"),
+        delim: 'a',
+    }
 }
 
 struct Config {
