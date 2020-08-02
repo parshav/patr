864c9c667099743fcadc8cde32b3b8631228dcc2
pv <parshav@live.com>
2020-08-02 00:50:32 -0500

    Just functions moved around

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,15 @@ fn main() {
     // check for current diredctory
     // Read argument
     // start the writing -> use read [option] or rust built in
+    // write out to file, file name should be title ?
 
-    // maybe better way to d this ?
+    let input = read_input_buffer();
+
+}
+
+// Get title from arg else give a default name maybe.
+fn get_title() -> String {
+	 // maybe better way to d this ?
     let title: String = env::args()
     	.into_iter()
     	.skip(1)
@@ -23,6 +30,12 @@ fn main() {
     	.expect("Error in getting argument");
 
     println!("Title is {}", title);
+
+    return title;
+}
+
+// Reads input buffer till delim '-'
+fn read_input_buffer() -> String {
 
 	let mut buffer = vec![];
 	let stdin = io::stdin();
@@ -32,17 +45,8 @@ fn main() {
     	.expect("Error in read until");
 
    	let buffer = str::from_utf8(&buffer).expect("error in utf8");
-   	println!("The buffer {}", buffer);
 
-   	/*buffer.iter()
-   	.map(|x| {
-   		str::from_utf8(x).expect("error in map")
-   	})
-   	.for_each(|x| {
-   		println("{}")	
-   	});*/
-
-	//println!("The buffer {:?}", buffer);
+   	return buffer.to_owned();
 }
 
 // checks for config, if exists, use that delim.
